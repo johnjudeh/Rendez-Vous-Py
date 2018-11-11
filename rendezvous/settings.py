c95588a535b6c22ae2384f517839c199da5e25d2
John Judeh <johnjudeh@hotmail.com>
2018-11-11 20:50:09 +0000

    feat: Add mapper to INSTALLED_APPS in settings.py

--- rendezvous/settings.py
+++ rendezvous/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mapper.apps.MapperConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
