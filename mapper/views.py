3979d82048904358aa9d2b44e4cdace7cce336e6
John Judeh <johnjudeh@hotmail.com>
2018-11-24 21:11:05 +0000

    feat: Add google maps API key which is stored as an environment variable

--- mapper/views.py
+++ mapper/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
+
+GOOGLE_MAPS_API_KEY = os.environ.get('MAPS_KEY')
 
 # Create your views here.
 def index(request):
-    return render(request, 'mapper/index.html')
+    return render(request, 'mapper/index.html', {'MAPS_KEY': GOOGLE_MAPS_API_KEY})
