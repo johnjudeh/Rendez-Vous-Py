380a2ba1f7cf9835eb1c5539b8bf646e54f81d00
John Judeh <johnjudeh@hotmail.com>
2018-11-24 19:30:10 +0000

    feat: Update mapper.views to render the mapper/index.html template & commented out incompatible template synyntax

--- mapper/views.py
+++ mapper/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('This is the maps page!')
+    return render(request, 'mapper/index.html')
