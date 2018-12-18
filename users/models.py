541700630eece18bbae3534ba157f36587ee76bc
John Judeh <johnjudeh@hotmail.com>
2018-12-18 20:30:05 +0000

    feat: Create RendezVousUser and Interest models  - Create Interest model with type and category fields using predefined list of choices  - Create the list of choices in a new constants dir  - Create RendezVousUser which inherits from auth.models.User adding a few extra fields and a many to many rel with Interest

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
+from .constants.interests import INTEREST_CATEGORY_CHOICES, INTEREST_TYPE_CHOICES
 
-# Create your models here.
+
+class Interest(models.Model):
+    type = models.CharField(max_length=50, choices=INTEREST_TYPE_CHOICES)
+    category = models.CharField(max_length=50, choices=INTEREST_CATEGORY_CHOICES)
+
+    def __str__(self):
+        return '{} - {}'.format(self.get_type_display(), self.get_category_display())
+
+
+class RendezVousUser(auth_models.User):
+    date_of_birth = models.DateField()
+    interests = models.ManyToManyField(Interest)
