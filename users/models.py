ed2007827dc8a64485913aea58e5318f54738e74
John Judeh <johnjudeh@hotmail.com>
2019-03-16 01:24:51 +0000

    feat: Add get_absolute_url method to users.Profile and use it instead of url in the base template

--- users/models.py
+++ users/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 from .constants.interests import INTEREST_CATEGORY_CHOICES, INTEREST_TYPE_CHOICES
 import json
 
 
 class Interest(models.Model):
+
     type = models.CharField(max_length=50, choices=INTEREST_TYPE_CHOICES, unique=True)
     category = models.CharField(max_length=50, choices=INTEREST_CATEGORY_CHOICES)
 
@@ -19,9 +21,11 @@ class User(AbstractUser):
 
 
 class Profile(models.Model):
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     interests = models.ManyToManyField(Interest)
 
+    # Methods on models add row-level functionality
     def get_interests_as_json(self):
         interest_list = []
         for interest in self.interests.all():
@@ -37,3 +41,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return self._get_interests_str()
+
+    def get_absolute_url(self):
+        """
+        Returns the specific url for a model instance.
+        A special purpose object method that is used in the admin app and can
+        be used in templates rather than specifically writing it out.
+        Try to always define this on your models.
+        """
+        return reverse('users:profile', args=[self.user.id])
