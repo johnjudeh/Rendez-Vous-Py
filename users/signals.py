79fec8f0be9d8972d773e609f62a99a195d1a3d6
John Judeh <johnjudeh@hotmail.com>
2019-03-16 21:42:39 +0000

    style: Add docstring to user post_save signal function that creates user profiles

--- users/signals.py
+++ users/signals.py
@@ -4,5 +4,6 @@ from .models import User, Profile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """Creates user profile when new user is created"""
     if created:
         Profile.objects.create(user=instance)
